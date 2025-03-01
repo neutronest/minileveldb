5ea2fa990fef9a7fa12d24a59df01a997a2e1711
neutronest <neutronest@gmail.com>
2025-03-02 00:22:12 +1100

    test project init

---
+++ minileveldb.test/FoobarTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace minileveldb.test
+{
+    [TestFixture]
+    public class FoobarTest
+    {
+        [Test]
+        public void TestFoobar()
+        {
+            var expectValue = true;
+            Assert.That(expectValue, Is.True);
+        }
+    }
+}
